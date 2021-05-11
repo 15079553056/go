4ca7d196d3b0788dae43714ee5c557c7c2ffd97c
15079553056 <1126438236@qq.com>
2021-05-11 16:15:20 +0800

    123

--- capybara/web/web_capybara/src/App.vue
+++ capybara/web/web_capybara/src/App.vue
@@ -2,7 +2,8 @@
 <div>
   <router-view></router-view>
   <div id="app">APP 根组件888</div>
-  
+  <router-link to="/login">登录页面</router-link>
+  <router-link to="/collect">收藏页面</router-link>
 </div>
 </template>
 
--- capybara/web/web_capybara/src/router/index.js
+++ capybara/web/web_capybara/src/router/index.js
@@ -1,33 +1,32 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Router from 'vue-router'
 import App from '../App'
 
-//安装路由
+//1. 安装路由插件
 Vue.use(Router)
 
 const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
 const login = r => require.ensure([], () => r(require('../page/login/login')), 'login')
 const collect = r => require.ensure([], () => r(require('../page/collect/collect')), 'collect')
 
-
-//路由配置
-export default new Router({
-  mode:'history',
-  routes:[
+//2. 创建 VueRouter 对象
+const router = new VueRouter({
+  routes: [
     {
-      path:'/',
-      name:"home",
-      component:home,
+      path: '/',
+      component: home,
     },
     {
-      path:'/collect',
-      name:'collect',
-      component:collect
+      path: '/collect',
+      component: collect
     },
     {
-      path:'/login',
-      name:'login',
-      component:login
+      path: '/login',
+      component: login
     }
   ]
 })
+
+//3. 将 router 对象传入到 Vue 实例中
+export default router
